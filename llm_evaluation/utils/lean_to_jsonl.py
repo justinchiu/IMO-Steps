f4c00c4eef93b16c834e6f3f84e87cb2566168b2
aziksh-ospanov <63187592+aziksh-ospanov@users.noreply.github.com>
2025-04-14 19:26:58 +0800

    Add files via upload

---
+++ llm_evaluation/utils/lean_to_jsonl.py
@@ -0,0 +1,91 @@
+import json
+import re
+    
+    
+
+def clean_set_option(text):
+    if 'set_option' in text:
+        return ''
+    else:
+        return text
+        
+
+def remove_comment(text):
+    idx = text.find('--')
+    if idx >= 0:
+        return text[:idx]
+    else:
+        return text
+
+def extract_proofs(filename, clean_comment_text=False):
+    with open(filename, 'r') as f:
+        txt = f.read()
+    blocks = re.split(r'(?=^(?:lemma|theorem)\b)', txt, flags = re.MULTILINE)
+    
+    header = blocks[0]
+    header = '\n'.join([clean_set_option(l) for l in header.split('\n')])
+    lst = []
+    
+    for block in blocks[1:]:
+        match = re.match(r'^(?:lemma|theorem)\s+(\S+)', block.strip())
+        name = match.group(1) if match else None
+        block = re.sub(r'\n+', '\n', block)
+        
+        # block = '\n'.join([remove_comment(l) for l in block.split('\n')])
+        if clean_comment_text:
+            block = '\n'.join([remove_comment(l) for l in block.split('\n') if remove_comment(l).strip() != ''])
+        
+        marker = ':= by'
+        idx = block.find(marker)
+        
+        formal_statement = block[:idx+len(marker)]
+        formal_proof = block[idx+len(marker):]
+        
+        t = {
+            'name' : name,
+            'split' : 'train',
+            'header' : header,
+            'informal_prefix' : '',
+            'formal_statement' : formal_statement,
+            'formal_proof' : formal_proof,
+            }
+        lst.append(t)
+    return lst
+        
+
+def lean_to_jsonl(leanfile, save_path = './test', clean_comment_text=False):
+    theorems = extract_proofs(leanfile, clean_comment_text=clean_comment_text)
+    
+    with open(save_path, 'w') as f:
+        for item in theorems:
+            f.write(json.dumps(item, ensure_ascii=False) + '\n')
+            
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+    
+        
